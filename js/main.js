8c1420d25fb5b9a04741e437425ff4fecdfcad3d
Ryan <corkery.ryan@gmail.com>
2018-12-11 19:06:12 +1100

    home page layout

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ function openHamburger(){
 	document.querySelector(".hamburger").style.display = "none";
 	document.querySelector(".links").style.display = "block";
 	document.querySelector(".social").style.display = "block";
-	// document.querySelector(".navRowLower").style.display = "block";
 	document.querySelector(".hamburgerClose").style.display = "block";
 }
 
@@ -15,7 +14,6 @@ function closeHamburger(){
 	document.querySelector(".hamburger").style.display = "block";
 	document.querySelector(".links").style.display = "none";
 	document.querySelector(".social").style.display = "none";
-	// document.querySelector(".navRowLower").style.display = "none";
 	document.querySelector(".hamburgerClose").style.display = "none";
 }
 
